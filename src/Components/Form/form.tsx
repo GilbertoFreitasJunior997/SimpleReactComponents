ad1d74bad8b3b24ce1ceb962363a1c82102066e2
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2022-05-29 14:01:03 -0300

    UseForm adjustments

--- src/Components/Form/form.tsx
+++ src/Components/Form/form.tsx
@@ -1,42 +1,36 @@
-import { FC, useRef } from "react";
+import { FC, FormEvent, ForwardedRef, forwardRef, MutableRefObject, useCallback, useImperativeHandle, useRef, useEffect } from 'react';
 import IFormProps from './formProps';
 
 const Form: FC<IFormProps> = ({ children }) => {
-    const { inputProps } = useForm<{
+    const { inputProps, handleSubmit } = useForm<{
         codigo: number;
         descricao: string;
-    }>({
-        initialValues: {
-            codigo: 20,
-            descricao: "asads",
-        }
-    });
+    }>();
 
-    const codName = "codigo";
-    const descName = "descricao";
+    const onSubmit = (data: any) => {
+        console.log("valid");
+    }
 
     return (
         <form
-            onSubmit={(e) => {
-                e.preventDefault();
-                console.log(inputProps.controller.current);
-            }}
+            onSubmit={async e => handleSubmit(e, onSubmit, () => console.log("invalid"))}
         >
             <Input
-                name={codName}
+                name="codigo"
                 placeholder="codigo"
                 type="number"
                 inputProps={inputProps}
             />
 
-            <input
-                name={descName}
+            <Input
+                name="descricao"
                 placeholder="descricao"
                 type="text"
-                onChange={(e) => {
-                    const val = e.target.value;
-                    if (val !== null && val !== undefined)
-                        inputProps.controller.current[descName] = val;
+                inputProps={inputProps}
+                validation={(val: string) => {
+                    if (val.length < 5) {
+                        return "desc < 5";
+                    }
                 }}
             />
 
@@ -52,45 +46,133 @@ const Form: FC<IFormProps> = ({ children }) => {
 
 export default Form;
 
+type ValidationModes = 'onSubmit' | 'onChange' | 'onBlur' | 'all';
+
 interface UseFormProps<T> {
-    mode?: 'onSubmit' | 'onChange' | 'onBlur' | 'all';
+    validationMode?: ValidationModes;
     initialValues?: T;
 };
-function useForm<T>({ mode, initialValues }: UseFormProps<T>) {
+function useForm<T>({ validationMode, initialValues }: UseFormProps<T> = {}) {
     const controller = useRef<T>(initialValues || ({} as T));
+    const inputsRefs = useRef<{
+        validate: (value: string) => string | void | Promise<string | void>,
+        value: string,
+        name: keyof T,
+    }[]>([]);
+
+    function getValue(inputName: keyof T): T[keyof T] {
+        return controller.current[inputName];
+    }
+
+    const handleSubmit = async (submitEvent: FormEvent<HTMLFormElement>, onSubmit?: (data: T) => void, onError?: () => void) => {
+        submitEvent.preventDefault();
+        let isValid = true;
+        inputsRefs.current.forEach(async input => {
+            const r = await input.validate(input.value || '');
+            if (typeof r !== "string") {
+                isValid = false;
+            }
+        })
+        if (isValid)
+            onSubmit && onSubmit(controller.current);
+        else
+            onError && onError();
+    }
 
     return {
+        getValue,
+        handleSubmit,
         inputProps: {
             controller,
-            mode: mode || 'onSubmit'
+            validationMode: validationMode || 'onSubmit',
+            inputsRefs,
         }
     };
+}
+
+interface IValidatableInput {
+    validate: (value: string) => Promise<string | void>;
+}
+
+interface InputRef extends IValidatableInput {
+
 }
 
 interface InputProps {
     name: string;
     inputProps: {
-        controller: React.MutableRefObject<any>;
-        mode: any;
+        controller: MutableRefObject<any>;
+        validationMode: ValidationModes;
+        inputsRefs: MutableRefObject<{
+            name: string;
+            validate(value: string): void;
+            value: string;
+        }[]>;
     }
 
+    validation?(value: string): string | void | Promise<string | void>;
     placeholder?: string;
     type?: string;
 }
 
-const Input: FC<InputProps> = ({ name, inputProps, ...props }) => {
+function InputComp({ name, inputProps, validation, ...props }: InputProps, ref: ForwardedRef<InputRef>) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const validate = useCallback(async (value: string) => {
+        // true = valid; false = invalid;
+        if (!validation) return;
+        else return await validation(value);
+    }, [validation])
+
+    const updateValue = useCallback((mode: ValidationModes, value?: string) => {
+        if (value !== null && value !== undefined) {
+            const thisInput = inputProps.inputsRefs.current.find(x => x.name === name)
+            if (thisInput) {
+                if (thisInput.value !== value) {
+                    inputProps.inputsRefs.current.map(x => {
+                        if (x.name === name)
+                            x.value = value;
+                        return x;
+                    })
+                }
+            }
+            else
+                inputProps.inputsRefs.current.push({ validate, value: inputProps.controller.current[name], name });
+
+            inputProps.controller.current[name] = value;
+
+            if (inputProps.validationMode === "all" || inputProps.validationMode === mode)
+                validate(value);
+        }
+    }, [inputProps, name, validate])
+
+    useImperativeHandle(ref, () => ({
+        validate,
+    }))
+
+    useEffect(() => {
+        const thisInput = inputProps.inputsRefs.current.find(x => x.name === name);
+        if (!thisInput)
+            inputProps.inputsRefs.current.push({ validate, value: inputProps.controller.current[name], name });
+    })
+
     return (
         <>
             <input
                 name={name}
+                ref={inputRef}
                 {...props}
                 defaultValue={inputProps.controller.current[name] || ""}
-                onChange={(e) => {
+                onChange={async (e) => {
                     const val = e.target.value;
-                    if (val !== null && val !== undefined)
-                        inputProps.controller.current[name] = val;
+                    updateValue("onChange", val)
+                }}
+                onBlur={(e) => {
+                    const val = e.target.value;
+                    updateValue("onBlur", val)
                 }}
             />
         </>
     )
 }
+
+const Input = forwardRef<IValidatableInput, InputProps>((props, inputRef) => InputComp(props, inputRef));
